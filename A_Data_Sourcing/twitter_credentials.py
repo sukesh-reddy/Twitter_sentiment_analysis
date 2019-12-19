4996d653725ef10bfd2242e3de21f863b1eb26f2
Sukesh <sukeshreddy97@gmail.com>
2019-12-19 23:51:17 +0530

    First Version

---
+++ A_Data_Sourcing/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "265312347-xd0QWmQfGPwn5mxkuuVCf5wVtwJI75zJw1IS8EmX"
+ACCESS_TOKEN_SECRET = "r5V55HUQRcJz11fWdnfDsoR6Ob6NkTf4e7K4QPdEnb1LW"
+CONSUMER_KEY = "JftwhrxaUeQqvBZF38b9am2hI"
+CONSUMER_SECRET = "TmExko21wbGqjSJdRjutzW4DtC3R0HAlI33ZLW832gQn9Ce8Ak"
